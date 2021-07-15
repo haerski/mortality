356e542432e124cefad68fac3461bab4f165cae0
Marc Härkönen <harkonen@gatech.edu>
2021-07-15 09:53:02 -0400

    finished deaths.R timeseries by zip3

--- data/deaths.R
+++ data/deaths.R
@@ -1,15 +1,22 @@
 library(tidyverse)
-deaths <- read_csv("covid_deaths_usafacts.csv")
-deaths <- deaths %>% select(1,5:542) %>% pivot_longer(2:539,names_to= "dates",values_to = "deaths")
-deaths$dates <- parse_date(deaths$dates)
+library(feather)
 
-deaths <- deaths %>% filter(countyFIPS!=0)
+zip3_rel <- read_feather("zip3_rel.feather")
 
-deaths %>% mutate(countyFIPS=as.character(countyFIPS)) %>% mutate(countyFIPS=str_pad(countyFIPS,5,pad="0")) %>% 
-  mutate(state=str_sub(countyFIPS,1,2),county=str_sub(countyFIPS,3,5)) %>% 
-  select(-countyFIPS)
+deaths <- read_csv("covid_deaths_usafacts.csv") %>%
+  mutate(countyFIPS = as.character(countyFIPS)) %>%
+  mutate(countyFIPS = str_pad(countyFIPS, 5, pad = "0")) %>%
+  mutate(state = str_sub(countyFIPS, 1, 2), county = str_sub(countyFIPS, 3, 5)) %>%
+  select(!c(`County Name`, State, StateFIPS, countyFIPS)) %>%
+  relocate(state, county)
 
+deaths_zip3_wide <- deaths %>%
+  inner_join(zip3_rel, by = c("state", "county")) %>%
+  group_by(zip3) %>%
+  summarize(across(`2020-01-22`:`2021-07-12`, ~ sum(.x * POPPT / sum(POPPT))))
 
-deaths %>% filter(countyFIPS==20045) %>% 
-  ggplot(aes(x=dates,y=deaths))+geom_line()
+deaths_zip3 <- deaths_zip3_wide %>%
+  pivot_longer(-1, names_to = "date", values_to = "deaths") %>%
+  mutate(date = parse_date(date))
 
+write_feather(deaths_zip3, "deaths_zip3.feather")
